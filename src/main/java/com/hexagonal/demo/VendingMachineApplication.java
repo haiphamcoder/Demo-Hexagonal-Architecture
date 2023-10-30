bbeef2d9f950fb13e14e3282c628e6ca9eefa43a
Hai Pham <ngochai285nd@gmail.com>
2023-10-30 09:59:24 +0700

    update

--- src/main/java/com/hexagonal/demo/VendingMachineApplication.java
+++ src/main/java/com/hexagonal/demo/VendingMachineApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoHexagonalArchitectureApplication {
+public class VendingMachineApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoHexagonalArchitectureApplication.class, args);
+        SpringApplication.run(VendingMachineApplication.class, args);
     }
 
 }
